98955eac62f694b8d01f8ce9ac6a3285af0e79d0
Sabrina Silva <silva.sabrina@escolar.ifrn.edu.br>
2026-06-19 15:54:19 -0300

    suap api

---
+++ tarefa14/suap.py
@@ -0,0 +1,26 @@
+import requests
+from getpass import getpass
+
+api_url = "https://suap.ifrn.edu.br/api/"
+
+user = input("user: ")
+password = getpass()
+
+data = {"username":user,"password":password}
+
+response = requests.post(api_url+"token/pair", json=data)
+token = response.json()["access"]
+print(response.json())
+
+token = ""
+
+headers = {
+    "Authorization": f'Bearer {token}'
+}
+
+print(headers)
+
+response = requests.get(api_url+"rh/eu/", headers=headers)
+
+print(response.text)
+print(response)
